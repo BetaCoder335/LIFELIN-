1e82ba71348ea7bd24d5abe0cc1d0e35f88217ea
BetaCoder335 <betacoder335@github.com>
2026-09-09 23:06:59 +0530

    Update Firebase config with new project credentials

--- config.js
+++ config.js
@@ -4,12 +4,12 @@ window.LifelineConfig = {
     API_BASE: "https://lifeline-backend.paawanparwani3653.workers.dev",
     
     FIREBASE: {
-        apiKey: "AIzaSyAvVbMdIHkkaSSC0V8ioq_A3lP5-ymPBo8",
-        authDomain: "lifeline-37b6b.firebaseapp.com",
-        projectId: "lifeline-37b6b",
-        storageBucket: "lifeline-37b6b.firebasestorage.app",
-        messagingSenderId: "157470955128",
-        appId: "1:157470955128:web:f872b991a3fca8116dff8d",
-        measurementId: "G-G3TF7CDDRL"
+        apiKey: "AIzaSyD9djUTr6Xow5kzDIt1eanf8MR2mNiJJSI",
+        authDomain: "lifeline-d8a92.firebaseapp.com",
+        projectId: "lifeline-d8a92",
+        storageBucket: "lifeline-d8a92.firebasestorage.app",
+        messagingSenderId: "976444057712",
+        appId: "1:976444057712:web:0f8210c4af22891f3fecb5",
+        measurementId: "G-T1F8WDPP62"
     }
 };
